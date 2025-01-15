ccbf85934bb1a81c44afe1b61377b849ca71b86f
Asleep <122379135+Asleep123@users.noreply.github.com>
2025-01-15 07:25:51 -0600

    Update number format

--- src/lib/data.tsx
+++ src/lib/data.tsx
@@ -42,7 +42,7 @@ export const sections: ContentSectionData[] = [
 	{
 		id: "testimonials",
 		title: "We Do It Best",
-		description: `750+ communities, over one million users.
+		description: `750+ communities, 1.000.000+ users.
 But don’t just take our word for it, hear from some of our users on what they have to say about Buape Studios:`,
 		cards: [
 			{
