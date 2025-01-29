679433fca0f9066d8ebb93d4611460420d4f9f25
Shadow <hi@shadowing.dev>
2025-01-28 18:19:24 -0600

    update opennext

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,9 @@
 import { createMDX } from "fumadocs-mdx/next"
 
+import { initOpenNextCloudflareForDev } from "@opennextjs/cloudflare";
+initOpenNextCloudflareForDev();
+
+
 /** @type {import("next").NextConfig} */
 const config = {
 	reactStrictMode: true,
